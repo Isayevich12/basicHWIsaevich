4acb8baedda6518a9e3389a7bf345610873770cb
dzmitry is <isayevich12@gmail.com>
2021-02-20 12:58:49 +0300

    Добавьте файлы проекта.

---
+++ basicHWIsaevich/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace basicHWIsaevich
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///явное преобразование типов c проверкой на переполнение и отловом исключения    
+            try
+            {
+                checked
+                {
+                    int a = 315;
+                    byte b = (byte)a;
+                    Console.WriteLine($"b=>{b}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message }");
+            }
+            
+
+
+
+
+        }
+    }
+}
