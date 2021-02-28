ce1b078e23a850dbc069e4ca614ee2f87c68a9cb
dzmitry is <isayevich12@gmail.com>
2021-02-28 10:23:25 +0300

    1

--- BasicHomeWork3/Program.cs
+++ BasicHomeWork3/Program.cs
@@ -2,11 +2,36 @@
 
 namespace BasicHomeWork3
 {
+
+
+
     class Program
     {
+        static string GetPrimeNumbersInRange(int rangeBorderMax)
+        {
+            string sequenseOfPrimeNubers = string.Empty;
+
+            while (rangeBorderMax > 0)
+            {
+                for (int i = 2; i < rangeBorderMax; i++)
+                {
+                    if (rangeBorderMax % i == 0)
+                    {
+                        continue;
+                    }
+
+                    sequenseOfPrimeNubers += rangeBorderMax.ToString() + " ";
+
+                }
+                rangeBorderMax--;
+            }
+
+            return sequenseOfPrimeNubers;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetPrimeNumbersInRange(13));
         }
     }
 }
